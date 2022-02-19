24c25ea130153cad93c4687187bcec8f8c2bda08
Patrick260 <Patrick260@protonmail.com>
2022-02-19 13:40:36 +0100

    Add text field

--- src/main/java/de/patrick260/jtexteditor/gui/GUI.java
+++ src/main/java/de/patrick260/jtexteditor/gui/GUI.java
@@ -74,6 +74,14 @@ public final class GUI extends JFrame {
 
         setJMenuBar(menuBar);
 
+        final JEditorPane editor = new JEditorPane();
+
+        final JScrollPane scrollPane = new JScrollPane(editor);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        add(scrollPane);
+
         pack();
 
         setLocationRelativeTo(null);
